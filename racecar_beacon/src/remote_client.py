70e5f8fbfb29656099017f3c789cb608e8b8bd83
Edouard5618 <92937471+Edouard5618@users.noreply.github.com>
2023-09-06 20:56:58 -0400

    First commit to test repo

--- racecar_beacon/src/remote_client.py
+++ racecar_beacon/src/remote_client.py
@@ -6,3 +6,17 @@ HOST = '127.0.0.1'
 # This process should listen to a different port than the PositionBroadcast client.
 PORT = 65432
 
+# Create a socket and connect to the server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4, TCP
+client.connect(HOST, PORT)
+
+# Send data to the server
+data = "Hello, Server!"
+client.send(data.encode())
+
+# Receive and print the server's response
+response = client.recv(1024).decode()
+print(f"Server response: {response}")
+
+# Close the client socket
+client.close()
