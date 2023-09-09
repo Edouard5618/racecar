f5ad2471331f920746e9394703e9a14289f35197
racecar <Charles.Belanger@usherbrooke.ca>
2023-09-09 16:13:25 -0400

    ajout de la fonction remoteRequestLoop dans rosMonitor

--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -31,38 +31,49 @@ class ROSMonitor:
         self.remote_request_port = rospy.get_param("remote_request_port", 65432)
         self.pos_broadcast_port  = rospy.get_param("pos_broadcast_port", 65431)
         self.broadcast_ip        = "255.255.255.255"
-        self.listen_ip           = "192.168.137.4"
-
+        self.listen_ip           = "192.168.137.215"
+        
+        print(self.remote_request_port)
+           
         # Thread for RemoteRequest handling:
         self.rr_thread = threading.Thread(target=self.remote_request_loop)
         self.pos_track_thread = threading.Thread(target=self.pos_track_loop)
 
+        self.rr_thread.start()
         print("ROSMonitor started.")
 
     def remote_request_loop(self):
         # Init your socket here :
+        print("IN LOOP")
         request_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4, TCP
+        print("after creation")
         request_socket.bind((self.listen_ip, self.remote_request_port))
+        print("after bind")
         request_socket.listen(1)
+        print("after listen")
         try:
             conn, addr = request_socket.accept()
             print('Remote Request connected by', addr)
             while True:
-                data = conn.recv(1024)
+                data = conn.recv(1024).decode()
                 if not data:
                     break
-                print("Received data: " + data.decode())
-                if data is "RPOS":
+                print("Received data: " + data)
+                if data == "RPOS":
+                    print("in RPOS")
                     conn.sendall((str(self.pos) + str(self.id)).encode())
-                elif data is "OBSF":
+                    print("send done")
+                elif data == "OBSF":
                     conn.sendall(str(self.obstacle).encode())
-                elif data is "RBID":
+                elif data == "RBID":
                     conn.sendall(str(self.id).encode())
-                elif data is "EXIT":
+                else:
+                    print("errors")
                     break
         finally:
             conn.close()
             request_socket.close()
+            print("Connection close")
         
 
                 
@@ -82,15 +93,16 @@ class ROSMonitor:
         #         while True:   
         #             conn.sendall((str(self.pos) + str(self.id)).encode())
         #             time.sleep(1)  # Diffusion toutes les 1 seconde
+        pass
 
 
     def odom_callback(self, msg):
-        rospy.loginfo("odom_callback: ")
-        rospy.loginfo(msg)
+        #rospy.loginfo("odom_callback: ")
+        # rospy.loginfo(msg)
 
         # Transform the Odometry message into a tuple (x, y, yaw):
-        rospy.loginfo("PARAMETRE_ORIENTATION: " + str(msg.pose.pose.orientation))
-        rospy.loginfo("PARAMETRE x: "+str(msg.pose.pose.position.x))
+        #rospy.loginfo("PARAMETRE_ORIENTATION: " + str(msg.pose.pose.orientation))
+        #rospy.loginfo("PARAMETRE x: "+str(msg.pose.pose.position.x))
         self.pos = (msg.pose.pose.position.x, msg.pose.pose.position.y, quaternion_to_yaw(msg.pose.pose.orientation))
 
         # Send the position to the remote client:
@@ -99,7 +111,7 @@ class ROSMonitor:
         pass
 
     def laser_callback(self, msg):
-        rospy.loginfo("laser_callback: " + str((msg.ranges)))
+        # rospy.loginfo("laser_callback: " + str((msg.ranges)))
 
         if min(msg.ranges) < 1000: # 1m
             self.obstacle = True
