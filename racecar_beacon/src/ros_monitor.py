711b1153b989c6d0489a47dab9a52520ace5fe1b
racecar <Charles.Belanger@usherbrooke.ca>
2023-09-10 16:10:06 -0400

    Formatage des données en binaire

--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -4,6 +4,7 @@ import rospy
 import socket
 import threading
 import time
+import struct
 
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
@@ -23,19 +24,19 @@ class ROSMonitor:
         self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.laser_callback)
 
         # Current robot state:
-        self.id = 0xFFFF
-        self.pos = (0,0,0)
-        self.obstacle = False
+        self.id = 0x0005
+        self.pos = (-9999,-9999,-9999)
+        self.obstacle = -1
 
-        #mutex
+        #mutex:
         self.mutexPos = threading.Lock()
         self.mutexObstacle = threading.Lock()
 
         # Params :
         self.remote_request_port = rospy.get_param("remote_request_port", 65432)
         self.pos_broadcast_port  = rospy.get_param("pos_broadcast_port", 65431)
-        self.listen_ip           = "192.168.137.215"
-        self.broadcast_ip = '255.255.255.255'
+        self.listen_ip           = "192.168.10.1"
+        self.broadcast_ip = '192.168.10.255'
         
         print(self.remote_request_port)
         # Thread for RemoteRequest handling:
@@ -52,32 +53,37 @@ class ROSMonitor:
         request_socket.bind((self.listen_ip, self.remote_request_port))
         request_socket.listen(1)
         try:
-            conn, addr = request_socket.accept()
-            print('Remote Request connected by', addr)
-            while rospy.is_shutdown() == False:
-                data = conn.recv(1024).decode()
-                if not data:
-                    break
-                print("Received data: " + data)
-                if data == "RPOS":
-                    self.mutexPos.acquire()
-                    conn.sendall((str(self.pos) + str(self.id)).encode())
-                    self.mutexPos.release()
-                elif data == "OBSF":
-                    self.mutexObstacle.acquire()
-                    conn.sendall(str(self.obstacle).encode())
-                    self.mutexObstacle.release()
-                elif data == "RBID":
-                    conn.sendall(str(self.id).encode())
-                else:
-                    print("errors")
-                    break
-            conn.close()
             
+            while rospy.is_shutdown() == False:
+                conn, addr = request_socket.accept()
+                print('Remote Request connected by', addr)
+                while rospy.is_shutdown() == False:
+                    data = conn.recv(1024).decode()
+                    if not data:
+                        break
+                    print("Received data: " + data)
+                    if data == "RPOS":
+                        self.mutexPos.acquire()
+                        packed_data= struct.pack("3f i",self.pos[0],self.pos[1],self.pos[2],0)
+                        conn.sendall(packed_data)
+                        self.mutexPos.release()
+                    elif data == "OBSF":
+                        self.mutexObstacle.acquire()
+                        packed_data= struct.pack("4i",self.obstacle,0,0,0)
+                        conn.sendall(packed_data)
+                        self.mutexObstacle.release()
+                    elif data == "RBID":
+                        packed_data= struct.pack("4i",self.id,0,0,0)
+                        conn.sendall(packed_data)
+                    else:
+                        print("errors")
+                        break
+                conn.close()
+                print("Connection close")
         finally:
             
             request_socket.close()
-            print("Connection close")
+            
         
 
     def pos_track_loop(self):
@@ -87,7 +93,8 @@ class ROSMonitor:
         count=0
         while rospy.is_shutdown() == False:
             self.mutexPos.acquire()
-            request_socket.sendto((str(self.pos) + str(self.id)).encode(), (self.broadcast_ip, self.pos_broadcast_port))
+            packed_data= struct.pack("3f i",self.pos[0],self.pos[1],self.pos[2],self.id)
+            request_socket.sendto(packed_data,(self.broadcast_ip, self.pos_broadcast_port))
             self.mutexPos.release()
             print("Broadcasting"+str(count))
             count=count+1
@@ -107,7 +114,9 @@ class ROSMonitor:
         # Transform the Odometry message into a tuple (x, y, yaw):
         #rospy.loginfo("PARAMETRE_ORIENTATION: " + str(msg.pose.pose.orientation))
         #rospy.loginfo("PARAMETRE x: "+str(msg.pose.pose.position.x))
+        self.mutexPos.acquire()
         self.pos = (msg.pose.pose.position.x, msg.pose.pose.position.y, quaternion_to_yaw(msg.pose.pose.orientation))
+        self.mutexPos.release()
 
         # Send the position to the remote client:
         # self.rr_socket.send(...)
@@ -119,11 +128,11 @@ class ROSMonitor:
 
         if min(msg.ranges) < 1000: # 1m
             self.mutexObstacle.acquire()
-            self.obstacle = True
+            self.obstacle = 1
             self.mutexObstacle.release()
         else:
             self.mutexObstacle.acquire()
-            self.obstacle = False
+            self.obstacle = 0
             self.mutexObstacle.release()
 
 if __name__=="__main__":
