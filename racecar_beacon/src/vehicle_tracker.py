62f410ff1bca4ad3eea9aa2628829d58ad2ea64b
racecar <edouard5618@gmail.com>
2023-09-10 16:14:41 -0400

    resolve bugs and encode with struct

--- racecar_beacon/src/vehicle_tracker.py
+++ racecar_beacon/src/vehicle_tracker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import socket
+import struct
 
 HOST = ''
 # This process should listen to a different port than the PositionBroadcast client.
@@ -11,15 +12,17 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPv4, UDP
 client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 client.bind((HOST, PORT))
-
+print("Wait for message")
 # Receive and print the server's response
 while True:
-    print("Wait for message")
     response, source_address = client.recvfrom(1024)
-    print("Server response:"+ str(response.decode()))
+    response = struct.unpack("3f i", response)
+    pos = response[:3]
+    ID = response[3]
+    print("Server response pos: "+ str(pos) + " and ID: "+ str(ID))
+    
     if response == "EXIT":
         break
-
 
 
 # Close the client socket
